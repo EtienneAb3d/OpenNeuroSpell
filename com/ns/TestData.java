630eed7c3017529c4d55f3e93fdc71811bbcd214
EtienneAb3d <em@lingua-et-machina.com>
2019-05-19 16:49:04 +0200

    Improved + first draft rule set for English forked from French

--- com/ns/TestData.java
+++ com/ns/TestData.java
@@ -1,7 +1,7 @@
 package com.ns;
 
 public class TestData {
-	static final String lng = "en";
+	static final String lng = "fr";
 	static final String text = 
 			"Longtemps, les gens se sont dit \"on leur fait confiance, ils vont bien trouver quelque chose\" et à la COP24, le GIEC a dit qu'il fallait revenir sur les accords de Paris et ne surtout pas dépasser +1,5 °C de température. "
 			;
